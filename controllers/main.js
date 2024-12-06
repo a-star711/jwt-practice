d8b04ef9e3c66b17b7068025e3bc5503c7ddc754
a-star711 <a.krasimirov35@gmail.com>
2024-12-06 11:50:04 +0200

    CustomAPIerror validation

--- controllers/main.js
+++ controllers/main.js
@@ -1,4 +1,23 @@
+// check username, password in post(login) request
+// if exist create new JWT
+// send back to front-end
+
+// setup auth so only the request with JWT can access the dashboard
+
+
+const customAPIError = require('../errors/custom-error')
+
 const login = async (req,res) => {
+  const { username, password } = req.body;
+  // Multiple options for error handling:
+  // mongooose validations
+  // Joi package
+  // Check in the controller and throw error there
+  if(!username || !password) {
+   throw new customAPIError('Please provide email and password', 400)
+  }
+  console.log(username, password);
+
   res.send('Fake login/Register/Signup Route')
 }
 
