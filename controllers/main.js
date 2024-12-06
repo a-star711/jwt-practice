36244e30a446edaac8fbbcd2e3645c6d1fbc414b
a-star711 <a.krasimirov35@gmail.com>
2024-12-06 12:08:19 +0200

    Send JWT to front-end

--- controllers/main.js
+++ controllers/main.js
@@ -4,7 +4,7 @@
 
 // setup auth so only the request with JWT can access the dashboard
 
-
+const jwt = require('jsonwebtoken')
 const customAPIError = require('../errors/custom-error')
 
 const login = async (req,res) => {
@@ -16,9 +16,13 @@ const login = async (req,res) => {
   if(!username || !password) {
    throw new customAPIError('Please provide email and password', 400)
   }
-  console.log(username, password);
 
-  res.send('Fake login/Register/Signup Route')
+ // just for the demo
+  const id = new Date().getDate()
+
+  const token = jwt.sign({id, username}, process.env.JWT_SECRET, {expiresIn:'30d'} )
+
+   res.status(200).json({msg:'user created', token})
 }
 
 const dashboard = async (req,res) => {
